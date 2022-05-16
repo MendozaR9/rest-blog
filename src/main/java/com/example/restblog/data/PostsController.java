07d63a8d5d2e60610f477921c36b473565cc26d3
Ryan Mendoza <ryanmendoza.00@gmail.com>
2022-05-16 15:41:17 -0500

    create post contoller

--- src/main/java/com/example/restblog/data/PostsController.java
+++ src/main/java/com/example/restblog/data/PostsController.java
@@ -1,22 +1,27 @@
 package com.example.restblog.data;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;;
 
 @RestController
-@RequestMapping(value = "/api/posts", headers = "Accept=application/jason")
+@RequestMapping(value = "/api/posts", headers = "Accept=application/json")
 public class PostsController {
 
     @GetMapping
-    public List<Post> getAll(){
-        List<Post> newPosts = new ArrayList<>();
-        newPosts.add(new Post(1, "Dog pics", " this is a picture of my dog"));
-        newPosts.add(new Post(2,"Dinner", "Dinner "));
+    public ArrayList<Post> getAll() {
+        ArrayList<Post> newPosts = new ArrayList<>();
+        newPosts.add( new Post(1, "Dog pics", " this is a picture of my dog"));
+        newPosts.add(new Post(2, "Dinner", "Dinner "));
         return newPosts;
+    }
+
+    @GetMapping("/{id}")
+    public Post getById(@PathVariable long id){
+
+        return new Post(id, "Dinner", "Dinner ");
+
     }
 
 }
